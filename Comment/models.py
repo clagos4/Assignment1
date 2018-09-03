295eb0eeefa7173d808e401430d7bfa52e7836c3
clagos4 <clagos4@uc.cl>
2018-09-03 20:20:50 -0300

    migrations to create admin dashboard

--- Comment/models.py
+++ Comment/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class Comment(models.Model):
 
     comment = models.TextField()
-    author_ip = models.CharField()
+    author_ip = models.CharField(max_length=50)
     date = models.DateTimeField(auto_now_add=True)
