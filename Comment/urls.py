2a85a3651f5a4e3cf2fac46c2bf5a48e8a26712a
clagos4 <clagos4@uc.cl>
2018-09-03 22:48:02 -0300

    Comment create

--- Comment/urls.py
+++ Comment/urls.py
@@ -1,8 +1,7 @@
-from django.contrib import admin
 from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.list_comments),
-    path('new', create_products),
+    path('', views.list_comments, name="list_comments"),
+    path('new', views.create_comment, name="create_comment"),
 ]
--- Comment/views.py
+++ Comment/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Comment
+from .forms import CommentForm
+import logging
 
 def list_comments(request):
     comments = Comment.objects.all()
     return render(request, 'Comment/comment_list.html', { 'comments': comments })
+
+def create_comment(request):
+    logger = logging.getLogger(__name__)
+    form = CommentForm(request.POST or None)
+
+    if form.is_valid():
+        ip = request.META['REMOTE_ADDR']
+        form.author_ip = ip
+        form.save()
+        return redirect('list_comments')
+    
+    return render(request, 'Comment/comments-form.html', { 'form': form })
+    
